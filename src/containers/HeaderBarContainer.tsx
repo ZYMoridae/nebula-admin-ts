41d14a5f6fde2f0c8da2dc47f4a2a57a378a650d
Yue Zhou <ashzhouyue@gmail.com>
2019-11-16 08:28:46 +1100

    [FIX][Joe]: Tweak HeaderBarContainer

--- src/containers/HeaderBarContainer.tsx
+++ src/containers/HeaderBarContainer.tsx
@@ -1,203 +1,218 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import { fade } from '@material-ui/core/styles/colorManipulator';
-import { withStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import NebulaIcon from '../components/NebulaIcon';
-import Routes from '../utils/Routes';
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import Badge from "@material-ui/core/Badge";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import { withStyles } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MailIcon from "@material-ui/icons/Mail";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import NebulaIcon from "../components/NebulaIcon";
+import Routes from "../utils/Routes";
 
 // import ProductSearchComponent from '../components/ProductSearchComponent';
 
 import { withTranslation } from "react-i18next";
 
-import Constants from '../utils/Contants';
+import Constants from "../utils/Constants";
+
+import { Theme, createStyles } from "@material-ui/core";
 
 const drawerWidth = Constants.styles.sidebar.width;
 
-
-const styles = theme => ({
-  root: {
-    width: '100%',
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-  title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%"
     },
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
+    grow: {
+      flexGrow: 1
     },
-    marginRight: theme.spacing.unit * 2,
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing.unit * 3,
-      width: 'auto',
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20
     },
-  },
-  searchIcon: {
-    width: theme.spacing.unit * 9,
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-    width: '100%',
-    height: '100%'
-  },
-  inputInput: {
-    paddingTop: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingBottom: theme.spacing.unit,
-    paddingLeft: theme.spacing.unit * 10,
-    height: '100%',
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: theme.spacing.unit * 40,
+    title: {
+      display: "none",
+      [theme.breakpoints.up("sm")]: {
+        display: "block"
+      }
     },
-  },
-  sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
+    search: {
+      position: "relative",
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      "&:hover": {
+        backgroundColor: fade(theme.palette.common.white, 0.25)
+      },
+      marginRight: theme.spacing(2),
+      marginLeft: 0,
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        marginLeft: theme.spacing(3),
+        width: "auto"
+      }
     },
-  },
-  sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
+    searchIcon: {
+      width: theme.spacing(9),
+      height: "100%",
+      position: "absolute",
+      pointerEvents: "none",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
     },
-  },
-  homeButton: {
-    textDecoration: 'none',
-    color: 'white',
-    fontFamily: 'Megrim, cursive; !important'
-  },
-  myAppBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-  },
-  loginButton: {
-    color: 'white'
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
+    inputRoot: {
+      color: "inherit",
+      width: "100%",
+      height: "100%"
     },
-    marginRight: theme.spacing.unit * 2,
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing.unit * 2,
-      width: 'auto',
+    inputInput: {
+      paddingTop: theme.spacing(1),
+      paddingRight: theme.spacing(1),
+      paddingBottom: theme.spacing(1),
+      paddingLeft: theme.spacing(10),
+      height: "100%",
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("md")]: {
+        width: theme.spacing(40)
+      }
     },
-  },
-  searchIcon: {
-    width: theme.spacing.unit * 7,
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  notificationBadge: {
-    '& > span': {
-      backgroundColor: '#9a3300'
+    sectionDesktop: {
+      display: "none",
+      [theme.breakpoints.up("md")]: {
+        display: "flex"
+      }
+    },
+    sectionMobile: {
+      display: "flex",
+      [theme.breakpoints.up("md")]: {
+        display: "none"
+      }
+    },
+    homeButton: {
+      textDecoration: "none",
+      color: "white",
+      fontFamily: "Megrim, cursive; !important"
+    },
+    myAppBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: `${drawerWidth}px`
+    },
+    loginButton: {
+      color: "white"
+    },
+    // search: {
+    //   position: 'relative',
+    //   borderRadius: theme.shape.borderRadius,
+    //   backgroundColor: fade(theme.palette.common.white, 0.15),
+    //   '&:hover': {
+    //     backgroundColor: fade(theme.palette.common.white, 0.25),
+    //   },
+    //   marginRight: theme.spacing(2),
+    //   marginLeft: 0,
+    //   width: '100%',
+    //   [theme.breakpoints.up('sm')]: {
+    //     marginLeft: theme.spacing(2),
+    //     width: 'auto',
+    //   },
+    // },
+    // searchIcon: {
+    //   width: theme.spacing(7),
+    //   height: '100%',
+    //   position: 'absolute',
+    //   pointerEvents: 'none',
+    //   display: 'flex',
+    //   alignItems: 'center',
+    //   justifyContent: 'center',
+    // },
+    notificationBadge: {
+      "& > span": {
+        backgroundColor: "#9a3300"
+      }
     }
-  }
-});
+  });
 
-
-const renderLoginButton = (props) => {
+const renderLoginButton = (props: any) => {
   const { classes, t } = props;
 
-  let block = '';
+  let block: any = "";
 
   if (location.pathname !== Routes.USER.LOGIN) {
-    block = <div className={classes.sectionDesktop}>
-      <Typography variant="button" color="inherit" noWrap>
-        <a href={Routes.USER.LOGIN} className={classes.homeButton}>
-          {t('login_btn')}
-      </a>
-      </Typography>
-    </div>;
+    block = (
+      <div className={classes.sectionDesktop}>
+        <Typography variant="button" color="inherit" noWrap>
+          <a href={Routes.USER.LOGIN} className={classes.homeButton}>
+            {t("login_btn")}
+          </a>
+        </Typography>
+      </div>
+    );
   }
 
   return block;
-}
+};
 
-class PrimarySearchAppBar extends React.Component {
+type AppHeadBarState = {
+  anchorEl: any;
+  mobileMoreAnchorEl: any;
+  isUserLogin: boolean;
+};
+
+type AppHeadBarProps = {
+  classes: any;
+  t: any
+};
+
+class AppHeadBar extends React.Component<AppHeadBarProps, AppHeadBarState> {
   componentWillMount() {
     // TODO: user login check wrongly when network disconnected
     this.setState({
       anchorEl: null,
       mobileMoreAnchorEl: null,
-      isUserLogin: sessionStorage.getItem('user') != null && sessionStorage.getItem('user') != 'undefined' && sessionStorage.getItem('user') != 'null'
+      isUserLogin:
+        sessionStorage.getItem("user") != null &&
+        sessionStorage.getItem("user") != "undefined" &&
+        sessionStorage.getItem("user") != "null"
     });
   }
 
-  componentDidMount() {
+  handleProfileMenuOpen = (event: any) => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+  handleMenuClose = () => {
+    this.setState({ anchorEl: null });
+    this.handleMobileMenuClose();
+  };
 
+  handleMobileMenuOpen = (event: any) => {
+    this.setState({ mobileMoreAnchorEl: event.currentTarget });
+  };
 
-    this.handleProfileMenuOpen = event => {
-      this.setState({ anchorEl: event.currentTarget });
-    };
+  handleMobileMenuClose = () => {
+    this.setState({ mobileMoreAnchorEl: null });
+  };
 
-    this.handleMenuClose = () => {
-      this.setState({ anchorEl: null });
-      this.handleMobileMenuClose();
-    };
+  handleLogout = () => {
+    sessionStorage.removeItem("user");
+    sessionStorage.removeItem("token");
+    location.href = Routes.USER.LOGIN;
+  };
+  // componentDidMount() {
 
-    this.handleMobileMenuOpen = event => {
-      this.setState({ mobileMoreAnchorEl: event.currentTarget });
-    };
-
-    this.handleMobileMenuClose = () => {
-      this.setState({ mobileMoreAnchorEl: null });
-    };
-
-    this.handleLogout = () => {
-      sessionStorage.removeItem('user');
-      sessionStorage.removeItem('token');
-      location.href = Routes.USER.LOGIN;
-    }
-  }
+  // }
 
   render() {
     const { anchorEl, mobileMoreAnchorEl, isUserLogin } = this.state;
@@ -205,12 +220,11 @@ class PrimarySearchAppBar extends React.Component {
     const isMenuOpen = Boolean(anchorEl);
     const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-
     const renderMenu = (
       <Menu
         anchorEl={anchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
         open={isMenuOpen}
         onClose={this.handleMenuClose}
       >
@@ -223,8 +237,8 @@ class PrimarySearchAppBar extends React.Component {
     const renderMobileMenu = (
       <Menu
         anchorEl={mobileMoreAnchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
         open={isMobileMenuOpen}
         onClose={this.handleMenuClose}
       >
@@ -256,13 +270,12 @@ class PrimarySearchAppBar extends React.Component {
           </IconButton>
           <p>Log Out</p>
         </MenuItem>
-
       </Menu>
     );
 
-    const cartIconButtonClickHandler = (event) => {
-      window.location.href = '/cart';
-    }
+    const cartIconButtonClickHandler = (event: any) => {
+      window.location.href = "/cart";
+    };
 
     return (
       <div className={classes.root}>
@@ -271,12 +284,17 @@ class PrimarySearchAppBar extends React.Component {
             {/* {isUserLogin ? <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
               <MenuIcon />
             </IconButton> : ''} */}
-            <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+            <Typography
+              className={classes.title}
+              variant="h6"
+              color="inherit"
+              noWrap
+            >
               <Toolbar>
                 {/* <NebulaIcon /> */}
                 <Typography variant="h6" color="inherit" noWrap>
                   <a href="/" className={classes.homeButton}>
-                    {t('max_studio')}
+                    {t("max_studio")}
                   </a>
                 </Typography>
               </Toolbar>
@@ -294,10 +312,10 @@ class PrimarySearchAppBar extends React.Component {
               />
             </div> */}
             <div className={classes.grow} />
-            {isUserLogin ?
+            {isUserLogin ? (
               <div className={classes.sectionDesktop}>
                 {/* <ProductSearchComponent /> */}
-                            {/* <div className={classes.search}>
+                {/* <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
@@ -318,21 +336,34 @@ class PrimarySearchAppBar extends React.Component {
                   </Badge>
                 </IconButton> */}
                 <IconButton
-                  aria-owns={isMenuOpen ? 'material-appbar' : undefined}
+                  aria-owns={isMenuOpen ? "material-appbar" : undefined}
                   aria-haspopup="true"
-                  onClick={(e) => { this.handleProfileMenuOpen(e) }}
+                  onClick={e => {
+                    this.handleProfileMenuOpen(e);
+                  }}
                   color="inherit"
                 >
                   <AccountCircle />
                 </IconButton>
-              </div> : renderLoginButton(this.props)}
-            {isUserLogin ?
+              </div>
+            ) : (
+              renderLoginButton(this.props)
+            )}
+            {isUserLogin ? (
               <div className={classes.sectionMobile}>
-                <IconButton aria-haspopup="true" onClick={(e) => { this.handleMobileMenuOpen(e) }} color="inherit">
+                <IconButton
+                  aria-haspopup="true"
+                  onClick={e => {
+                    this.handleMobileMenuOpen(e);
+                  }}
+                  color="inherit"
+                >
                   <MoreIcon />
                 </IconButton>
-              </div> : ''
-            }
+              </div>
+            ) : (
+              ""
+            )}
           </Toolbar>
         </AppBar>
         {renderMenu}
@@ -342,8 +373,8 @@ class PrimarySearchAppBar extends React.Component {
   }
 }
 
-PrimarySearchAppBar.propTypes = {
-  classes: PropTypes.object.isRequired,
+AppHeadBar.propTypes = {
+  classes: PropTypes.object.isRequired
 };
 
-export default withTranslation()(withStyles(styles)(PrimarySearchAppBar));
+export default withTranslation()(withStyles(styles)(AppHeadBar));
